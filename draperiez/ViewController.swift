c0929181d3f841c7f9b634196b694c400ed493c8
Leo Dion <leogdion@brightdigit.com>
2017-05-29 10:56:35 -0400

    Initial Commit

---
+++ draperiez/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  draperiez
+//
+//  Created by Leo Dion on 5/29/17.
+//  Copyright © 2017 Leo Dion. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
