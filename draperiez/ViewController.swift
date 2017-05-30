52d6f1241e1ae7cb65c29008a012f715577dfd1d
Leo Dion <leogdion@brightdigit.com>
2017-05-30 13:16:41 -0400

    pulls list of windows

--- draperiez/ViewController.swift
+++ draperiez/ViewController.swift
@@ -7,25 +7,37 @@
 //
 
 import Cocoa
+import ScriptingBridge
 
 class ViewController: NSViewController {
-
+  
+  @IBOutlet weak var windowsPopUpButton: NSPopUpButton!
+  @IBOutlet weak var sizesPopUpButton: NSPopUpButton!
+  
+  var windows : [SystemEventsWindow]! = nil
+  
+  
   @IBAction func placeAction (_ button: NSButton) {
     
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     // Do any additional setup after loading the view.
+    let application = SBApplication(bundleIdentifier: "com.apple.systemevents")! as SystemEventsApplication
+    let processes = application.processes?().flatMap{ $0 as? SystemEventsProcess }
+    self.windows = processes?.flatMap{ $0.windows?().flatMap{ $0 as? SystemEventsWindow } }.flatMap{ $0 }
+    self.windowsPopUpButton.addItems(withTitles: windows.map{ $0.title ?? $0.name ?? "" })
+    
   }
-
+  
   override var representedObject: Any? {
     didSet {
-    // Update the view, if already loaded.
+      // Update the view, if already loaded.
     }
   }
-
-
+  
+  
 }
 
